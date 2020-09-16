56f183605c00aec556a84cd0f30b3fd10923f0fe
github username <github email address>
2020-09-16 13:34:14 -0400

    Finished Comment

---
+++ js/challenge.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(e){
+    function leaveComment(){
+        const form = document.querySelector("#comment-form")
+        const submitButton = document.querySelector("button#submit");
+
+        form.addEventListener('submit', function(e){
+            e.preventDefault()
+            const inputText = e.target.querySelector('input').value
+            const div = document.createElement("div")
+            div.innerHTML = `<p>${inputText}</p>`
+            const parentDiv = document.querySelector("#list")
+            parentDiv.append(div) 
+        })
+    }
+    leaveComment()
+})
